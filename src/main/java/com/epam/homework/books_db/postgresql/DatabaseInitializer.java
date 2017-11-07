7efd83f8434f3e7e5c3110c6932c04397e98b998
Efim Poberezkin <efim@poberezkin.ru>
2017-11-07 21:49:41 +0400

    Added a script for database initialisation

--- src/main/java/com/epam/homework/books_db/postgresql/DatabaseInitializer.java
+++ src/main/java/com/epam/homework/books_db/postgresql/DatabaseInitializer.java
@@ -1,13 +1,20 @@
 package com.epam.homework.books_db.postgresql;
 
+import org.apache.ibatis.jdbc.ScriptRunner;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseInitializer {
+
+    private static final String INITIALIZATION_SCRIPT = "src\\main\\resources\\sql\\db_initialization.sql";
 
     private static final Logger log = Logger.getRootLogger();
 
@@ -31,13 +38,13 @@ public class DatabaseInitializer {
             stmt = conn.createStatement();
 
             //drops database to perform a clean test
-            stmt.executeUpdate("DROP DATABASE " + DB_NAME);
+            //stmt.executeUpdate("DROP DATABASE " + DB_NAME);
 
             String sql = "CREATE DATABASE " + DB_NAME;
             stmt.executeUpdate(sql);
             log.debug("Database created");
         } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e); //TODO: log instead
+            log.error("Failed to create database", e);
         } finally {
             try {
                 if (stmt != null) {
@@ -57,48 +64,21 @@ public class DatabaseInitializer {
 
     private static void executeInitializationScript() {
         Connection conn = null;
-        Statement stmt = null;
         try {
             log.debug("Connecting to database...");
             conn = DriverManager.getConnection(URL + DB_NAME, USER, PASSWORD);
 
             log.debug("Creating tables...");
-            stmt = conn.createStatement();
-
-            stmt.executeUpdate("CREATE TABLE author (id SERIAL NOT NULL, name varchar NOT NULL, dateOfBirth date NOT NULL, dateOfDeath date, genderId int4 NOT NULL, PRIMARY KEY(id))");
-            stmt.executeUpdate("CREATE TABLE gender (id SERIAL NOT NULL, name varchar NOT NULL, PRIMARY KEY(id))");
-            stmt.executeUpdate("CREATE TABLE book (id SERIAL NOT NULL, name varchar NOT NULL, yearOfPublication int4 NOT NULL, PRIMARY KEY(id))");
-            stmt.executeUpdate("CREATE TABLE booksAuthors (id SERIAL NOT NULL, bookId int4 NOT NULL, authorId int4 NOT NULL)");
-            stmt.executeUpdate("CREATE TABLE publisher (id SERIAL NOT NULL, name varchar NOT NULL, PRIMARY KEY(id))");
-            stmt.executeUpdate("CREATE TABLE publishersBooks (id SERIAL NOT NULL, publisherId int4 NOT NULL, bookId int4 NOT NULL)");
-            stmt.executeUpdate("CREATE TABLE dataset (id SERIAL NOT NULL, PRIMARY KEY(id))");
-            stmt.executeUpdate("CREATE TABLE datasetsAuthors (id SERIAL NOT NULL, datasetId int4 NOT NULL, authorId int4 NOT NULL)");
-            stmt.executeUpdate("CREATE TABLE datasetsBooks (id SERIAL NOT NULL, datasetId int4 NOT NULL, bookId int4 NOT NULL)");
-            stmt.executeUpdate("CREATE TABLE datasetsPublishers (id SERIAL NOT NULL, datasetId int4 NOT NULL, publisherId int4 NOT NULL)");
-            stmt.executeUpdate("ALTER TABLE author ADD CONSTRAINT author_uq UNIQUE (name, dateOfBirth, genderId)");
-            stmt.executeUpdate("ALTER TABLE book ADD CONSTRAINT book_uq UNIQUE (name, yearOfPublication)");
-            stmt.executeUpdate("ALTER TABLE publisher ADD CONSTRAINT publisher_uq UNIQUE (name)");
-            stmt.executeUpdate("ALTER TABLE author ADD CONSTRAINT Ref_Author_to_Gender FOREIGN KEY (genderId) REFERENCES gender(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE booksAuthors ADD CONSTRAINT Ref_Book_has_Author_to_Book FOREIGN KEY (bookId) REFERENCES book(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE booksAuthors ADD CONSTRAINT Ref_Book_has_Author_to_Author FOREIGN KEY (authorId) REFERENCES author(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE publishersBooks ADD CONSTRAINT Ref_Publisher_has_Book_to_Publisher FOREIGN KEY (publisherId) REFERENCES publisher(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE publishersBooks ADD CONSTRAINT Ref_Publisher_has_Book_to_Book FOREIGN KEY (bookId) REFERENCES book(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE datasetsAuthors ADD CONSTRAINT Ref_Dataset_has_Author_to_Dataset FOREIGN KEY (datasetId) REFERENCES dataset(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE datasetsAuthors ADD CONSTRAINT Ref_Dataset_has_Author_to_Author FOREIGN KEY (authorId) REFERENCES author(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE datasetsBooks ADD CONSTRAINT Ref_Dataset_has_Book_to_Dataset FOREIGN KEY (datasetId) REFERENCES dataset(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE datasetsBooks ADD CONSTRAINT Ref_Dataset_has_Book_to_Book FOREIGN KEY (bookId) REFERENCES book(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE datasetsPublishers ADD CONSTRAINT Ref_Dataset_has_Publisher_to_Dataset FOREIGN KEY (datasetId) REFERENCES dataset(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("ALTER TABLE datasetsPublishers ADD CONSTRAINT Ref_Dataset_has_Publisher_to_Publisher FOREIGN KEY (publisherId) REFERENCES publisher(id) MATCH SIMPLE ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE");
-            stmt.executeUpdate("INSERT INTO gender(id, name) VALUES (1, \'MALE\'), (2, \'FEMALE\')");
+            ScriptRunner sr = new ScriptRunner(conn);
+            sr.setLogWriter(null);
+            Reader reader = new BufferedReader(new FileReader(INITIALIZATION_SCRIPT));
+            sr.runScript(reader);
 
             log.debug("Tables created");
-        } catch (SQLException e) {
-            throw new RuntimeException(e); //TODO: log instead
+        } catch (IOException | SQLException e) {
+            log.error("Failed to execute database initialization script", e);
         } finally {
             try {
-                if (stmt != null) {
-                    stmt.close();
-                }
                 if (conn != null) {
                     conn.close();
                     log.debug("Connection closed");
---
+++ src/main/resources/sql/db_initialization.sql
@@ -0,0 +1,156 @@
+CREATE TABLE author (
+	id SERIAL NOT NULL,
+	name varchar NOT NULL,
+	dateOfBirth date NOT NULL,
+	dateOfDeath date,
+	genderId int4 NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE gender (
+	id SERIAL NOT NULL,
+	name varchar NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE book (
+	id SERIAL NOT NULL,
+	name varchar NOT NULL,
+	yearOfPublication int4 NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE booksAuthors (
+	id SERIAL NOT NULL,
+	bookId int4 NOT NULL,
+	authorId int4 NOT NULL
+);
+
+CREATE TABLE publisher (
+	id SERIAL NOT NULL,
+	name varchar NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE publishersBooks (
+	id SERIAL NOT NULL,
+	publisherId int4 NOT NULL,
+	bookId int4 NOT NULL
+);
+
+CREATE TABLE dataset (
+	id SERIAL NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE datasetsAuthors (
+	id SERIAL NOT NULL,
+	datasetId int4 NOT NULL,
+	authorId int4 NOT NULL
+);
+
+CREATE TABLE datasetsBooks (
+	id SERIAL NOT NULL,
+	datasetId int4 NOT NULL,
+	bookId int4 NOT NULL
+);
+
+CREATE TABLE datasetsPublishers (
+	id SERIAL NOT NULL,
+	datasetId int4 NOT NULL,
+	publisherId int4 NOT NULL
+);
+
+
+ALTER TABLE author
+    ADD CONSTRAINT author_uq
+    UNIQUE (name, dateOfBirth, genderId);
+
+ALTER TABLE book
+    ADD CONSTRAINT book_uq
+    UNIQUE (name, yearOfPublication);
+
+ALTER TABLE publisher
+    ADD CONSTRAINT publisher_uq
+    UNIQUE (name);
+
+ALTER TABLE author ADD CONSTRAINT Ref_Author_to_Gender FOREIGN KEY (genderId)
+	REFERENCES gender(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE booksAuthors ADD CONSTRAINT Ref_Book_has_Author_to_Book FOREIGN KEY (bookId)
+	REFERENCES book(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE booksAuthors ADD CONSTRAINT Ref_Book_has_Author_to_Author FOREIGN KEY (authorId)
+	REFERENCES author(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE publishersBooks ADD CONSTRAINT Ref_Publisher_has_Book_to_Publisher FOREIGN KEY (publisherId)
+	REFERENCES publisher(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE publishersBooks ADD CONSTRAINT Ref_Publisher_has_Book_to_Book FOREIGN KEY (bookId)
+	REFERENCES book(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE datasetsAuthors ADD CONSTRAINT Ref_Dataset_has_Author_to_Dataset FOREIGN KEY (datasetId)
+	REFERENCES dataset(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE datasetsAuthors ADD CONSTRAINT Ref_Dataset_has_Author_to_Author FOREIGN KEY (authorId)
+	REFERENCES author(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE datasetsBooks ADD CONSTRAINT Ref_Dataset_has_Book_to_Dataset FOREIGN KEY (datasetId)
+	REFERENCES dataset(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE datasetsBooks ADD CONSTRAINT Ref_Dataset_has_Book_to_Book FOREIGN KEY (bookId)
+	REFERENCES book(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE datasetsPublishers ADD CONSTRAINT Ref_Dataset_has_Publisher_to_Dataset FOREIGN KEY (datasetId)
+	REFERENCES dataset(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE datasetsPublishers ADD CONSTRAINT Ref_Dataset_has_Publisher_to_Publisher FOREIGN KEY (publisherId)
+	REFERENCES publisher(id)
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+
+INSERT INTO gender (id, name)
+    VALUES (1, 'MALE'), (2, 'FEMALE');
